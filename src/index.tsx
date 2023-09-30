b14b04ff52e1d338374f2848bffb104279f99923
firas <firas.moukhallalaty@gmail.com>
2023-09-30 15:04:30 +0200

    added vercel analytics testing

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +14,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
