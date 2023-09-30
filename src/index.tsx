a33afc824265bead7248a98293a9d171daaf2963
firas <firas.moukhallalaty@gmail.com>
2023-09-30 15:13:22 +0200

    testing analytics

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Analytics />
-    </BrowserRouter>
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+    <Analytics />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
